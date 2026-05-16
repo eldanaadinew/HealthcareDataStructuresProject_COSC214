eb36dd452792eba2e4d3510444786828fdbb6f08
eldanaadinew <ADINEWE1216@students.bowiestate.edu>
2026-05-16 12:17:24 -0400

    Update DynamicArrayStructure.java

--- DynamicArrayStructure.java
+++ DynamicArrayStructure.java
@@ -1,1 +1,64 @@
+public class DynamicArrayStructure {
+    private PatientRecord[] records;
+    private int size;
 
+    public DynamicArrayStructure() {
+        records = new PatientRecord[10];
+        size = 0;
+    }
+
+    public void insertRecord(PatientRecord record) {
+        if (size == records.length) {
+            resize();
+        }
+
+        records[size] = record;
+        size++;
+    }
+
+    public PatientRecord searchRecord(int recordID) {
+        for (int i = 0; i < size; i++) {
+            if (records[i].getRecordID() == recordID) {
+                return records[i];
+            }
+        }
+
+        return null;
+    }
+
+    public boolean deleteRecord(int recordID) {
+        for (int i = 0; i < size; i++) {
+            if (records[i].getRecordID() == recordID) {
+                for (int j = i; j < size - 1; j++) {
+                    records[j] = records[j + 1];
+                }
+
+                records[size - 1] = null;
+                size--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void traverseRecords() {
+        for (int i = 0; i < size; i++) {
+            PatientRecord record = records[i];
+        }
+    }
+
+    private void resize() {
+        PatientRecord[] newRecords = new PatientRecord[records.length * 2];
+
+        for (int i = 0; i < records.length; i++) {
+            newRecords[i] = records[i];
+        }
+
+        records = newRecords;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
